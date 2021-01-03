c33da06e6891cb608bfdb28f659c6f605020ee7c
Ryan Draves <dravesr@umich.edu>
2021-01-03 18:40:40 -0500

    updated gitignore

--- ros-noetic/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uma/UMA/MarBot/ros-noetic/src
-# Build directory: /home/uma/UMA/MarBot/ros-noetic/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("marbot_teleop")
-subdirs("marbot_teleop_controller")
--- ros-noetic/build/marbot_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uma/UMA/MarBot/ros-noetic/src/marbot_teleop
-# Build directory: /home/uma/UMA/MarBot/ros-noetic/build/marbot_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
